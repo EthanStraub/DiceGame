369ea05e0effff8dcb459748adc2d95ea1c746a3
EthanStraub <ethan.straub@gmail.com>
2018-11-29 14:50:20 -0600

    Full version released. All notable bugs fixed, with an additional feature being the ability to check how much fatigue you have after defending, represented by the color of the defend button.

--- DiceJava.js
+++ DiceJava.js
@@ -17,17 +17,20 @@ var enemyAppeared = false;
 var anticipationDMG = 0;
 
 var mysteryDMGproto = "";
-var mysteryDMG = [];
+var mysteryDMG = ['?1'];
 var mysteryDMGalpha = 0;
 var mysteryDMGbeta = 0;
 var enemy = 0;
 var enemyType = ["dwarf", "goblin", "demon"];
-var enemyHP = [20,40,60];
+var enemyHP = [10,30,50];
 var enemyData = [];
 
 //Mechanics
 var defense = false;
 var fatigue = 0;
+var massFatigue = 0;
+var fatigueDeath = false;
+var fatigueColor = false;
 var tired = false;
 var playerDamage = 0;
 var anticipationDMG = 0;
@@ -44,6 +47,7 @@ var d6crit = false;
 var d8stockpile = 0;
 var d8totalstocks = 0;
 var d8limit = 2;
+var d8trigger = false;
 //--d12--
 var d12recoil = false;
 var recoilCalc = false;
@@ -87,7 +91,7 @@ function initialize() {
         sideNumProto = radios[i].value;
         sideNum = sideNumProto.replace(/\s/g, '');
         sideInt = parseInt(sideNum);
-        var initMsg = "You enter the dungeon with your "+sideNum+"-sided die!";
+        var initMsg = "You enter the dungeon with your "+sideNum+"-sided die!<br /><br />";
         var initMsgHP = "<span style='color:yellow'>Floor: </span> "+enemyTypeFloor+" <span style='color:red'>HP: </span> "+playerHp+" <span style='color:fuchsia'>Anticipated enemy damage: </span> ["+mysteryDMG+"]";
         document.getElementById("combatBox").innerHTML = initMsg;
         document.getElementById("effectBox").innerHTML = initMsgHP;
@@ -150,8 +154,9 @@ function rollDie() {
   } else if (d6crit) {
     playerDamage = Math.floor(((Math.random() * 6) + 1) * 3);
   } else if (d8totalstocks > 0) {
-    playerDamage = Math.floor(((Math.random() * 8) * d8totalstocks + 1));
+    playerDamage = Math.floor(((Math.random() * 8) + 1) * d8totalstocks);
     d8totalstocks = 0;
+    d8limit = 2;
   } else if (d12recoil) {
     playerDamage = Math.floor(((Math.random() * sideNum) + 1));
     recoilCalc = true;
@@ -161,7 +166,6 @@ function rollDie() {
     d8totalstocks = 0;
     d8limit = 2;
   }
-  //console.log(playerDamage)
   damageCalc();
   difficultyIncr();
   endOfTurn();
@@ -229,11 +233,10 @@ function sideEffect() {
       d8stockpile += 1;
       if (d8stockpile >= d8limit) {
         d8stockpile = 0;
-        d8totalstocks +=1;
+        d8totalstocks +=2;
         d8limit += 0.5;
-        effectMsg = "Defend successful! D-8 stockpiled energy for a total of "+d8totalstocks+". <br>Its next roll will expend energy for "+d8totalstocks+"x damage!";
-      } else {
-        effectMsg = "Defend successful!";;
+        d8trigger = true;
+        effectMsg = "D-8 stockpiled energy for a total of "+d8totalstocks+"! <br>Its next roll will expend energy for "+d8totalstocks+"x damage!";
       }
     }
   //D12 effect
@@ -256,6 +259,26 @@ function restoreHP() {
   playerHp+=playerRestore;
 }
 
+function defendRecolor() {
+  if (fatigueColor) {
+    document.getElementById("defendButton").style.color = "black";
+    document.getElementById("defendButton").style.borderColor = "black";
+    document.getElementById("defendButton").style.backgroundColor = "white";
+  } else if (fatigue <= 0 ) {
+    document.getElementById("defendButton").style.color = "#00ffff";
+    document.getElementById("defendButton").style.borderColor = "#00ffff";
+    document.getElementById("defendButton").style.backgroundColor = "#000099";
+  } else if (fatigue > 0 && fatigue <= 1) {
+    document.getElementById("defendButton").style.color = "yellow";
+    document.getElementById("defendButton").style.borderColor = "yellow";
+    document.getElementById("defendButton").style.backgroundColor = "#ff6600";
+  } else if (fatigue > 1 && fatigue <= 2) {
+    document.getElementById("defendButton").style.color = "red";
+    document.getElementById("defendButton").style.borderColor = "red";
+    document.getElementById("defendButton").style.backgroundColor = "#800000";
+  }
+}
+
 function enemyAttack() {
   if (enemyData[0] == "dwarf") {
     enemyDamage = Math.floor((Math.random() * 5) + 1)
@@ -272,12 +295,12 @@ function anticipate() {
     anticipationDMG = enemyDamage;
     foresight = false;
   } else {
-    anticipationDMG = 4
+    anticipationDMG = 1
   }
 }
 
 function mystery() {
-  mysteryDMG = [];
+  mysteryDMG = ['?1'];
   mysteryDMGproto = anticipationDMG.toString();
   mysteryDMGproto = mysteryDMGproto.split("");
   if (anticipationDMG < 10) {
@@ -299,12 +322,16 @@ function damageCalc() {
   summon();
   enemyAttack();
   if (recoilCalc) {
-    effectMsg = "D-12 damaged itself! It took an additional "+playerDamage+" damage!";
+    effectMsg = "D-12 damaged itself! It took an additional "+playerDamage+" damage!<br>";
     anticipationDMG+=playerDamage;
     playerDamage = 0;
   }
   if (defense && (!tired)) {
-    combatMsg = "You defend against the "+enemyData[0]+"'s attack of "+anticipationDMG+" damage!";
+    combatMsg = "You defend against the "+enemyData[0]+"'s attack of "+anticipationDMG+" damage!<br /><br />";
+    if (d8trigger) {
+      d8trigger = false;
+      combatMsg = "You defend against the "+enemyData[0]+"'s attack of "+anticipationDMG+" damage!";
+    }
     anticipationDMG = 0;
   } else if ((enemyData[1]-playerDamage) <= 0) {
     anticipationDMG = 0;
@@ -323,19 +350,21 @@ function damageCalc() {
     }
     restoreHP();
     if (!tired && sideNum !== 20) {
-      combatMsg = "You attacked the "+enemyData[0]+" for "+playerDamage+" damage and restored "+playerRestore+" HP! It fights back with "+anticipationDMG+" damage!";
+      combatMsg = "You attacked the "+enemyData[0]+" for "+playerDamage+" damage and restored "+playerRestore+" HP!<br> It fights back with "+anticipationDMG+" damage!";
       if (d20attempt) {
-        combatMsg = "D-20 took "+anticipationDMG+" damage from the "+enemyData[0];
+        combatMsg = "D-20 took "+anticipationDMG+" damage from the "+enemyData[0]+"<br /><br />";
         playerDamage = 0;
         d20attempt = false;
         tired = false;
         fatigue = 0;
       }
     } else if (tired && sideNum !== 20) {
-      combatMsg = "D-"+sideNum+" got tired of defending and took "+anticipationDMG+" damage!";
+      combatMsg = "D-"+sideNum+" got tired of defending and took "+anticipationDMG+" damage!<br /><br />";
       tired = false;
+      massFatigue+=1;
     } else if (tired && (sideNum == 4 && sideNum !== 20)) {
       combatMsg = "D-4 got tired of defending and took "+anticipationDMG+" damage!";
+      massFatigue+=1;
     }
     if (recoilCalc) {
       combatMsg = "You take a combined total of "+anticipationDMG+" damage!";
@@ -345,9 +374,11 @@ function damageCalc() {
 }
 
 function difficultyIncr() {
-  turn+=1;
-  if (turn%10===0) {
-    enemyTypeFloor+=1;
+  if (!isGameOver) {
+    turn+=1;
+    if (turn%10===0) {
+      enemyTypeFloor+=1;
+    }
   }
 }
 
@@ -359,17 +390,28 @@ function gameOver() {
 }
 
 function endOfTurn() {
+  defendRecolor();
   if (!isGameOver) {
-    //null
+    if (massFatigue >= 5){
+      isGameOver = true;
+      fatigueDeath = true;
+    }
   }
   if (isGameOver) {
     //null
     playerHp = 0;
-    combatMsg = "You have perished! Press restart to try again!";
-    effectMsg = " <span style='color:red'>HP: </span> "+playerHp;
+    if (fatigueDeath) {
+      fatigueColor = true;
+      defendRecolor();
+      combatMsg = "You have perished due to fatigue! Press restart to try again!<br /><br />";
+      fatigue = 0;
+    } else {
+      combatMsg = "The "+enemyData[0]+" slayed you! Press restart to try again!<br /><br />";
+    }
+    effectMsg = "<span style='color:yellow'>Floor: </span> "+enemyTypeFloor+" <span style='color:red'>HP: </span> "+playerHp;
   } else if (!enemyAppeared) {
     summon();
-    effectMsg = "Next Enemy: "+enemyData[0]+" <span style='color:red'>HP: </span> "+playerHp;
+    effectMsg = "Next Enemy: "+enemyData[0]+" <span style='color:yellow'>Floor: </span> "+enemyTypeFloor+" <span style='color:red'>HP: </span> "+playerHp+"<br> <span style='color:fuchsia'>Anticipated enemy damage: </span> [??]";
     anticipate();
     mystery();
   } else {
